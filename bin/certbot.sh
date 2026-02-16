94998b259909a039b4d7e7c683a560d10a887b8a
Afonso de Mori <me@afonso.dev>
2026-02-16 22:14:08 +0000

    feat: Use DHI for nginx

--- bin/certbot.sh
+++ bin/certbot.sh
@@ -6,6 +6,10 @@ cmd="$1"
 docker compose run --rm -i certbot "$@"
 
 case "$cmd" in
+  new|renew)
+    docker compose run --rm -T --entrypoint=/bin/sh certbot -c 'chown -R 65532:65532 /etc/letsencrypt'
+    make nginx-reload
+    ;;
   new|renew|revoke|delete)
     make nginx-reload
     ;;
